14f449795a16bb73e109f06b118dfc98df3f593e
talha.shoaib <talha@yallamotor.com>
2018-05-24 00:25:07 +0500

    changed db/gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 20170531165702) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
-    t.integer "user_id"
+    t.bigint "user_id"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -32,4 +35,5 @@ ActiveRecord::Schema.define(version: 20170531165702) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
+  add_foreign_key "messages", "users"
 end
